a69ec6717df8b1dc25c4cf1267df55c9e3ce62be
Alexandre Werneck <xanwerneck@gmail.com>
2021-06-06 11:51:04 -0300

    Start a new branch on chain of responsibility

---
+++ DesignPatternsTutorialTest/ChainOfResponsibilityTest.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using DesignPatternsTutorial.Strategy;
+using DesignPatternsTutorial.Strategy.Methods;
+
+namespace DesignPatternsTutorialTest
+{
+    public class ChainOfResponsibilityTest {
+        [Fact]
+        public void Start()
+        {            
+            /*
+            Participation participation = new Participation();
+            Publication publication     = new Publication(participation);
+            TypePayment typePayment     = new TypePayment(publication);
+
+            participation.Emit(new User{
+                Name = "UserExample"
+            });
+            */
+        }
+    }
+}
