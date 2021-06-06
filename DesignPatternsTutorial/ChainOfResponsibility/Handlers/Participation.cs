081307f8c4888e654af4c9e2f7047b99434179c3
Alexandre Werneck <xanwerneck@gmail.com>
2021-06-06 15:58:54 -0300

    Code for chain of responsibility, after i gonna refer any post to explain important aspects of pattern

---
+++ DesignPatternsTutorial/ChainOfResponsibility/Handlers/Participation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsTutorial.ChainOfResponsibility.Handlers
+{
+    public class Participation : Certificate
+    {
+        public override object Emit(Inscription inscription)
+        {
+            if (inscription.Status != "Paid")
+            {
+                return "Inscription not paid";
+            }
+            return base.Emit(inscription);
+        }
+    }
+}
---
+++ DesignPatternsTutorial/ChainOfResponsibility/Inscription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternsTutorial.ChainOfResponsibility
+{
+    public class Inscription
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Value { get; set; }
+        public List<string> Papers { get; set; }
+        public string Status { get; set; }
+        public int Watched { get; set; }
+    }
+}
